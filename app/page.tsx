77561c519f9909121c9788054e8c261bf0eeea1e
TuborJan <TuborJan@yandex.ru>
2023-06-13 16:37:43 +0300

    feat: add filter options

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CountriesList from "@/components/CountriesList";
 import { getData } from "@/service/getCountries";
 
 const Countries = async () => {
-  const countries: object = await getData();
+  const countries: object[] = await getData();
 
   return (
     <>
--- components/CountriesList.tsx
+++ components/CountriesList.tsx
@@ -6,29 +6,87 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { Select, Option } from "@material-tailwind/react";
 
 interface IProps {
-  countries: any;
+  countries: object[];
 }
+
+interface ICustomMaterialTailwindTheme {
+  select: {
+    styles: {
+      base: {
+        container?: object;
+        select?: object;
+        label?: object;
+        menu?: object;
+        option?: {
+          initial?: object;
+          active?: object;
+          disabled?: object;
+        };
+      };
+    };
+  };
+}
+
+const customTheme: ICustomMaterialTailwindTheme = {
+  select: {
+    styles: {
+      base: {
+        container: {
+          position: "relative",
+          maxWidth: "max-w-[100px]",
+        },
+        select: {
+          border: "border-none",
+          bg: "bg-white",
+        },
+        label: {
+          after: "after:border-none",
+          before: "before:border-none",
+        },
+      },
+    },
+  },
+};
 
 const CountriesList = ({ countries }: IProps) => {
   const [search, setSearch] = useState<string>("");
+  const [select, setSelect] = useState<string>("");
+
+  const handleChange = (e: any) => {
+    setSelect(e);
+  };
 
   return (
-    <ThemeProvider>
+    <ThemeProvider value={customTheme}>
       <div className="container max-w-screen-mobile px-2">
         <input
-          className="w-full mt-6 px-4 py-4 rounded-md"
+          className="w-full mt-6 px-4 py-2 rounded-md"
           type="search"
           placeholder="Search for a country..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Select label="test">
-          <Option>Test</Option>
-        </Select>
+        <div className="mt-8">
+          <Select
+            color="blue-gray"
+            label="Filter by Region"
+            onChange={handleChange}
+          >
+            <Option value="">All</Option>
+            <Option value="Africa">Africa</Option>
+            <Option value="Americas">America</Option>
+            <Option value="Asia">Asia</Option>
+            <Option value="Europe">Europe</Option>
+            <Option value="Oceania">Oceania</Option>
+          </Select>
+        </div>
         <ul className="container grid grid-cols-1 gap-10 max-w-xs mt-10 px-6">
           {countries
             .filter((country: any) =>
               country.name.common.toLowerCase().includes(search.toLowerCase())
+            )
+            .filter((country: any) =>
+              select !== "" ? country.region === select : true
             )
             .map((country: any) => (
               <li
